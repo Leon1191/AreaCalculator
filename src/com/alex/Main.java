086a93176289e9cf5bb2bff849a81d5c9b54cda3
Leo <stbirb@yandex.ru>
2019-05-09 15:31:08 +0300

    Initial commit

---
+++ src/com/alex/Main.java
@@ -0,0 +1,31 @@
+package com.alex;
+
+public class Main
+{
+    private static final double ERROR = -1d;
+//    public static void main(String[] args)
+//    {
+//        System.out.println(area(-5));
+//        System.out.println(area(2,3));
+//    }
+public static double area (double radius)
+{
+    if (radius < 0)
+    {
+        return ERROR;
+    }
+    return 3.14159 * radius*radius;
+}
+    public static double area (double x, double y)
+    {
+        if ((x < 0) || (y < 0))
+        {
+            return ERROR;
+        }
+        return (x * y);
+    }
+
+
+
+
+}
